f61230179db9bbbd15b72884e3fe0e6f53bc39f9
Muma Dev <muma.sanmartin2011@gmail.com>
2022-06-18 00:06:38 -0300

    submenu not working propertly

--- index.js
+++ index.js
@@ -11,14 +11,30 @@ dropdowns.forEach(dropdowns => {
     const options = dropdowns.querySelectorAll('.menu li')
     const selected = dropdowns.querySelector('.selected')
 
+    const submenu = dropdowns.querySelector('.submenu')
+
+    // console.log(submenus)
+
     
 
     //add a click event to the select element
     select.addEventListener('click', () => {
         //add clicked select style to the select element
+        
+        const unselectMenus = document.querySelectorAll('.menu-open')
+        const unselectClick = document.querySelectorAll('.select-clicked')
+        
         selected.classList.toggle('select-clicked')
-        console.log(menu)
         menu.classList.toggle('menu-open')
+
+
+        unselectMenus.forEach(menu =>{
+            menu.classList.remove('menu-open')
+        })
+
+        unselectClick.forEach(click =>{
+            click.classList.remove('select-clicked')
+        })
     })
 
 
@@ -26,18 +42,27 @@ dropdowns.forEach(dropdowns => {
     options.forEach(option => {
         //add a click event to the option element
         option.addEventListener('click', () => {
+
             //change selected inner text to the clicked option text
-            selected.innerText = option.innerText
+            // selected.innerText = option.innerText
+
             //add the clicked select styles to the select elements
-            selected.classList.remove('select-clicked')
+            // selected.classList.remove('select-clicked')
+
             //add the open styles to the menu element
-            menu.classList.remove('menu-open')
+            // menu.classList.remove('menu-open')
+
             //remove active class from all option elements
             options.forEach(option => {
                 option.classList.remove('active')
             })
+
             //add active class to clicked option element
             option.classList.add('active')
+
+
+            //testing submenu
+            submenu.classList.add('sub-menu-open')
         })
     })
 })
